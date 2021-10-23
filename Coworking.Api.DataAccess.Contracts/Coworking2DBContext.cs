c10d8d455e55b5803b862f41b9d27f353f61b845
Your Name <sergio.jn024@gmail.com>
2021-10-22 23:52:40 -0500

    Configuracion DbContext

---
+++ Coworking.Api.DataAccess.Contracts/Coworking2DBContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAccess.Contracts
+{
+    public interface Coworking2DBContext
+    {
+    }
+}
---
+++ Coworking.Api.DataAccess/CoworkingDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Coworking.Api.DataAccess.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAccess
+{
+    class CoworkingDBContext : DbContext, Coworking2DBContext
+    {
+        public CoworkingDBContext()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
